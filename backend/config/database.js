1c2a8a1d1fd33ec64c303cb4231b25d4c3bd0d41
estelle <estellewall@gmail.com>
2021-06-24 12:07:51 -0700

    set env variables

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,29 +1,32 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-//const port = process.env.PORT || 5000;
+const host = process.env.DB_HOST || 'localhost';
+const database = process.env.DB_NAME || 'mouse_inventory';
+const user = process.env.DB_USER || 'mouseuser';
+const password = process.env.DB_PASSWORD || 'password';
 const mariadb = require('mariadb');
 
 //localhost
-/*
+
 const pool = mariadb.createPool({
      
-     host:'localhost', 
-     database:'mouse_inventory',
-     user:'mouseuser', 
-     password:'password',
+     host, 
+     database,
+     user, 
+     password,
      connectionLimit: 5
      }  
 
 );
 
-*/
-
+/*
 const pool = mariadb.createPool({
      host: 'process.env.DB_HOST', 
      database:'process.env.DB_NAME',
      user: 'process.env.DB_USER', 
      password: 'process.env.DB_PASSWORD',
      connectionLimit: 5 });
+*/
 pool.getConnection()
     .then(conn => {
       console.log("connected ! connection id is " + conn.threadId);
