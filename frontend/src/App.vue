2ceab135be37d302e2dc9c8bea49ec27a0f292e5
estelle <estellewall@gmail.com>
2021-07-25 12:13:52 -0700

    Title and layout fixes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,14 +6,22 @@
          <button class="ucsf-header" style="padding-left:15px" @click="redirect('http://www.ucsf.edu')">University of California San Francisco</button>
         </template>
         <template #right>
-         
           <button class="ucsf-header" @click="redirect('http://www.ucsf.edu/about')">About UCSF</button>
           <button class="ucsf-header" @click="redirect('http://www.ucsf.edu/search')" title="">Search UCSF</button>
           <button class="ucsf-header" @click="redirect('http://www.ucsfhealth.org/')">UCSF Health</button>
         </template>
     </Toolbar>
-        <h2 style="text-align:center">UCSF MOUSELIST where researchers can find collaborators with similar mouse needs and share costs.</h2>
-<h3 style="text-align:center">Didn't find what you're looking for? Search the <a href="https://mousedatabase.ucsf.edu/search.jsp">UCSF Mouse Database</a>
+    <Toolbar class="ucsf-toolbar" style="height:120px; border:none; background-color:#FFF;">
+      <template #left >
+         <h2 style="font-family:Arial; font-size:26px; color:#052049">UCSF Mouselist</h2>
+        </template>
+        <template #right>
+          <button class="ucsf-nav" @click="redirect('http://mousedatabase.ucsf.edu')">Mouse Inventory</button>
+          <button class="ucsf-nav" @click="redirect('https://larc.ucsf.edu')" >LARC</button>
+        </template>
+    </Toolbar>
+        <h2 style="text-align:center">MOUSELIST where researchers find collaborators with similar mouse needs and share costs.</h2>
+<h3 style="text-align:center">Didn't find what you're looking for? Search the <a href="https://mousedatabase.ucsf.edu/search.jsp">UCSF Mouse Inventory Database</a>
 <img src='./assets/target_mice.png' class= "target-mice"></h3>
         <Button label="About" style="background-color:#178CCB; margin-left: 8px" icon="pi pi-external-link" class="p-button-rounded" @click="openAbout()" />
         <Dialog position= "topright" header="About UCSF Mouselist" v-model:visible="aboutDialog" :style="{width: '50vw'}">
@@ -49,7 +57,7 @@
             <Button
               label="Posts"
               icon="pi pi-flag"
-              style="background-color:#178CCB"
+              style="background-color:#178CCB; margin-left: 8px"
               class="p-button-rounded"
               @click="refreshPosts"
             />
@@ -126,7 +134,16 @@ export default {
   color:#FFF;
   font-size: 14px;
   cursor: pointer;
+  pointer-events:auto;
   
+}
+.ucsf-nav{
+  color:#052049;
+  border: none;
+  background-color:#FFF;
+  font-size: 14px;
+  cursor: pointer;
+  pointer-events: auto;
 }
 
 </style>
